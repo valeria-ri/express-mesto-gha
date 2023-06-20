20cebf942d5543e131e7a3c4d7033440601513ca
Valeria Rybakova <valerian.rybakova@yandex.ru>
2023-06-20 23:38:29 +0300

    fix: прописать обработку ошибки неправильного пути

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes');
+const { notFoundError } = require('./utils/errors');
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
 
 const { PORT = 3000 } = process.env;
 
@@ -19,5 +22,7 @@ app.use((req, res, next) => {
 });
 
 app.use(router);
+
+app.use('*', (req, res) => res.status(notFoundError).send({ message: 'Задан неправильный путь' }));
 
 app.listen(PORT);
