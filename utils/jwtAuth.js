223f27d0d1e723d3e684fbca1ab187dbe0a766e8
Valeria Rybakova <valerian.rybakova@yandex.ru>
2023-07-07 00:09:56 +0300

    feat: ограничить срок действия токена

--- utils/jwtAuth.js
+++ utils/jwtAuth.js
@@ -3,7 +3,7 @@ const { SECRET_KEY } = require('./constants');
 
 const checkToken = (token) => jwt.verify(token, SECRET_KEY);
 
-const signToken = (payload) => jwt.sign(payload, SECRET_KEY);
+const signToken = (payload) => jwt.sign(payload, SECRET_KEY, { expiresIn: '7d' });
 
 module.exports = {
   checkToken,
