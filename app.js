5c5cc3f547306c181e8f4100f5bffa8b17b2142d
Valeria Rybakova <valerian.rybakova@yandex.ru>
2023-07-12 15:55:35 +0300

    изменить место хранени ключа

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
@@ -7,24 +8,32 @@ const { NotFoundError } = require('./errors/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const internalServerErrorHandler = require('./middlewares/internalServerErrorHandler');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(express.json());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(router);
+
+app.use('*', (req, res, next) => next(new NotFoundError('Задан неправильный путь')));
 
 app.use(errorLogger);
 
 app.use(errors());
-app.use('*', (req, res, next) => next(new NotFoundError('Задан неправильный путь')));
+
 app.use(internalServerErrorHandler);
 
 app.listen(PORT);
